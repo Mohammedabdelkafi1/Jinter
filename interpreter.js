41032dc608365fb74331aced9b3580662dbed9d5
Mohammed abdelkafi <kawruulife@gmail.com>
2025-03-07 15:41:09 +0100

    Updated interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -1,1 +1,14 @@
+const prompt = require("prompt-sync")({ sigint: true });
 
+class Lexer {
+    constructor(text) {
+        this.src = text.split('');
+        this.pos = -1;
+        this.tokens = [];
+        this.curr_char = None;
+    }
+    advance() {
+        this.pos+=1;
+        this.curr_char = this.src[pos];
+    }
+}
